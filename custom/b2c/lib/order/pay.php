11c28866f884e6805577b98ca756be3d09dadef4
liwq <nick@pzfresh.com>
2015-02-15 12:24:39 +0800

    经验值 提交

--- custom/b2c/lib/order/pay.php
+++ custom/b2c/lib/order/pay.php
@@ -68,14 +68,12 @@ class b2c_order_pay extends b2c_api_rpc_request
                                         $obj->generate_bills($sdf, $objOrderbills, $sdf['pay_type'], $this->str_op_id, $this->str_op_name, $errorMsg);
                                     }
                                 }
+								
+								// 增加经验值
+								$obj_member = $this->app->model('members');
+								$obj_member->change_exp($objOrderbills['rel_id'], floor($objOrderbills['money']));
+								
                             }
-                            
-                            
-                            file_put_contents('f.log', serialize($sdf)."\r\n".serialize($objOrderbills)."\r\n",FILE_APPEND);
-                            // 增加经验值
-                            $obj_member = $this->app->model('members');
-                            $obj_member->change_exp($objOrderbills['rel_id'], floor($objOrderbills['money']));
-                            
                             break;
                         case 'joinfee':                            
                             break;
