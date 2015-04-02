eb420dd9f2ebfae373ea2fd739502ae44c9c6c5d
sam <sam@qq.com>
2015-04-02 12:59:42 +0800

    门店日结页面充值笔数统计bug修复

--- custom/ome/model/dailycash.php
+++ custom/ome/model/dailycash.php
@@ -23,7 +23,7 @@ class ome_mdl_dailycash extends dbeav_model{
 		$total = array();
 		$branch_where = '';
 		if ($branch_id>0) {
-			$branch_where = "and branch_id='{$branch_id}'";
+			$branch_where = " and branch_id='{$branch_id}' ";
 		}
 		
 		$rs = $this->db->select("select count(*) as _count,sum(final_amount) as price from sdb_b2c_orders where shipping='门店收银' and pay_status != '4' and pay_status != '5' ".$branch_where." and FROM_UNIXTIME( createtime, '%Y-%m-%d' ) = '{$date_line}' ");
@@ -61,6 +61,21 @@ class ome_mdl_dailycash extends dbeav_model{
 		$total['no_pmt_num'] = $rs[0]['_count']; //没优惠订单数
 		$total['no_pmt_price'] = $rs[0]['price'];//没优惠订单总金额
 		
+		
+		$rs = $this->db->select("select count(*) as _count,sum(final_amount) as price,payment from sdb_b2c_orders where shipping='门店自提' and pay_status in ('1','2') and ship_status in ('1','2')  ".$branch_where." and FROM_UNIXTIME( createtime, '%Y-%m-%d' ) = '{$date_line}' ");
+		
+		$total['store_num'] = $rs[0]['_count']; //门店自提订单数
+		$total['store_price'] = $rs[0]['price'];//门店自提订单总金额
+		foreach ($total as $key=>$value) {
+			if (empty($value)) {
+				$total[$key] = 0;
+			}
+		}
+		
+		
+		if ($branch_where  == '') {
+			$branch_where = ' and branch_id > 0 ';
+		}
 		//刷卡充值
 		$rs = $this->db->select("SELECT COUNT(*) AS _count,SUM(money) AS money,paymethod FROM  sdb_b2c_member_advance  WHERE FROM_UNIXTIME( mtime, '%Y-%m-%d' ) = '{$date_line}' ".$branch_where." and import_money > 0  GROUP BY paymethod");
 		$total['advance_xianjin_num'] = 0;//现金充值笔数
@@ -72,26 +87,17 @@ class ome_mdl_dailycash extends dbeav_model{
 		
 		foreach ($rs as $key=>$value) {
 			if ($value['paymethod'] == 'xianjin') {
-				$total['advance_xianjin_num'] = $value['_count'];
-				$total['advance_xianjin_money'] = $value['money'];
+				$total['advance_xianjin_num'] = $rs[0]['_count'];
+				$total['advance_xianjin_money'] = $rs[0]['money'];
 			} else if ($value['paymethod'] == 'shuaka') {
-				$total['advance_shuaka_num'] = $value['_count'];
-				$total['advance_shuaka_money'] = $value['money'];
+				$total['advance_shuaka_num'] = $rs[0]['_count'];
+				$total['advance_shuaka_money'] = $rs[0]['money'];
 			} else if ($value['paymethod'] == 'chongzhiquan') {
-				$total['advance_chongzhiquan_num'] = $value['_count'];
-				$total['advance_chongzhiquan_money'] = $value['money'];
+				$total['advance_chongzhiquan_num'] = $rs[0]['_count'];
+				$total['advance_chongzhiquan_money'] = $rs[0]['money'];
 			}
 		}
 		
-		$rs = $this->db->select("select count(*) as _count,sum(final_amount) as price,payment from sdb_b2c_orders where shipping='门店自提' and pay_status in ('1','2') and ship_status in ('1','2')  ".$branch_where." and FROM_UNIXTIME( createtime, '%Y-%m-%d' ) = '{$date_line}' ");
-		
-		$total['store_num'] = $rs[0]['_count']; //门店自提订单数
-		$total['store_price'] = $rs[0]['price'];//门店自提订单总金额
-		foreach ($total as $key=>$value) {
-			if (empty($value)) {
-				$total[$key] = 0;
-			}
-		}
 		return $total;
 	}
 	
