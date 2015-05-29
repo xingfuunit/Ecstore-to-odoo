efc6ee41fbc8764f3783ac56379da74e08978e04
sam <sam@qq.com>
2015-05-29 15:35:14 +0800

    61活动

--- custom/b2c/controller/site/active.php
+++ custom/b2c/controller/site/active.php
@@ -59,6 +59,7 @@ class b2c_ctl_site_active extends b2c_frontpage{
     	  		'vipday',//会员日-海鲜类（seafood）
     	  		'vipday_meat',//会员日-肉类（meat）
     	  		'vipday_fruit',//会员日-水果（fruit）
+    	  		'childrenday',//儿童节
     	  		);
           if(in_array($active_name,$active)){
           	$this->page('site/active/'.$active_name.'/index.html',true);//活动页面全屏，不要head和foot
