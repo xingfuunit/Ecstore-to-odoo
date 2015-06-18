31891cafdcc263e20273150c8a398b033906e7a6
liwq <nick@pzfresh.com>
2015-06-18 23:34:22 +0800

    热门列表 增加了

--- custom/b2c/controller/wap/gallery.php
+++ custom/b2c/controller/wap/gallery.php
@@ -138,7 +138,7 @@ class b2c_ctl_wap_gallery extends wap_frontpage{
    					'is_buildexcerpts'=>"true",
     			);
     	
-    	$goodsData = $goodsModel->getList('*',$filter,0,10);
+    	$goodsData = $goodsModel->getList('*',$filter,0,12);
     	foreach($goodsData as $key=>$goods_row){
     		if(in_array($goods_row['goods_id'],$gfav)){
     			$goodsData[$key]['is_fav'] = 'true';
