ec4b7a8030bcb455c78699e65330364231b4c368
july <july@pzfresh.com>
2015-07-25 10:54:11 +0800

    门店触屏，管理后台，添加预览功能

--- custom/mobileapi/lib/finder/sales/touchscreendevice.php
+++ custom/mobileapi/lib/finder/sales/touchscreendevice.php
@@ -11,6 +11,12 @@ class mobileapi_finder_sales_touchscreendevice{
     function column_edit($row){
         return '<a href="index.php?app=mobileapi&ctl=admin_sales_touchscreendevice&act=edit&_finder[finder_id]='.$_GET['_finder']['finder_id'].'&p[0]='.$row['device_id'].'" target="_blank">'.app::get('b2c')->_('编辑').'</a>';
     }
-	
-
+    var $column_view = '预览';
+    function column_view($row){
+		if (isset($_SERVER['SERVER_NAME']) && strlen(''.$row['device_name'])>0 ){
+			$url=$_SERVER['SERVER_NAME']; 
+			return '<a href="http://'.$url.'/wap/touchscreen.html?uuid='.$row['device_name'].'" target="_blank">'.app::get('b2c')->_('预览').'</a>';
+		}
+        return '';
+    }
 }
